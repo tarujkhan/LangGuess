6c1d772b9a4e9f262e17b9c706c2fd265666b1ea
tarujkhan <tarujkhan@yahoo.com>
2022-04-27 15:41:22 -0500

    First repository created for NODE

---
+++ index.js
@@ -0,0 +1,9 @@
+import franc from "franc";
+console.log(franc);
+// import langs, { where } from "langs";
+// console.log(langs);
+// const input = process.argv[2];
+// const langCode = franc(input);
+
+// const language = where("3", langCode);
+// console.log(language.name);
